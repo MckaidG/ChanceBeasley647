f4f867cb5c6cf31431ea18fe82561f4eedfc85c5
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-09-17 19:10:35 -0600

    hello

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -80,7 +80,7 @@ class BlogsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
         params.require(:blog).permit(:title,
-            :body,
+            :body
        )
     end
 
